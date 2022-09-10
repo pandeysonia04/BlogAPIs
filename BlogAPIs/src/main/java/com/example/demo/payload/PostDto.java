f6aaad9c390df419a8cdc10e0c7011de1abaaa64
pandeysonia04 <sonypandey371@gmail.com>
2022-09-10 23:37:39 +0530

    Post APIs

---
+++ BlogAPIs/src/main/java/com/example/demo/payload/PostDto.java
@@ -0,0 +1,20 @@
+package com.example.demo.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+	
+	private Long id ;
+	private String title;
+	private String description;
+	private String content;
+	
+	
+	
+
+}
