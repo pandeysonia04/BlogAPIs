e9b4be2b54b90d5f335ccd2d52e3e908de0b752c
pandeysonia04 <sonypandey371@gmail.com>
2022-09-11 19:05:59 +0530

    Comment Entity functionality completed

---
+++ BlogAPIs/src/main/java/com/example/demo/exception/BlogAPIException.java
@@ -0,0 +1,31 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BlogAPIException extends RuntimeException {
+	
+	private HttpStatus status;
+	private String message;
+	
+	public BlogAPIException(String message, HttpStatus status, String message1) {
+		super(message);
+		this.status = status;
+		this.message = message;
+		}
+	
+	public BlogAPIException(HttpStatus status, String message1) {
+	this.status = status;
+		this.message = message1;
+	}
+	
+	
+	public HttpStatus getStatus() {
+		return status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	
+	
+
+}
